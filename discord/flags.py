e6cac6f3bad4527faf1e3eb0bef69f30c6f544a7
Anay0305 <100482615+Anay0305@users.noreply.github.com>
2022-11-11 09:27:15 +0530

    Update flags.py

--- discord/flags.py
+++ discord/flags.py
@@ -409,11 +409,12 @@ class PublicUserFlags(BaseFlags):
         .. versionadded:: 2.0
         """
         return UserFlags.discord_certified_moderator.value
-    @flag_value def active_developer(self):
-    """:class:`bool`: Returns ``True`` if the user is an active developer. 
+    @flag_value
+    def active_developer(self):
+        """:class:`bool`: Returns ``True`` if the user is an active developer. 
     
-    .. versionadded:: 2.1 """ 
-    return UserFlags.active_developer.value
+        .. versionadded:: 2.1 """ 
+        return UserFlags.active_developer.value
 
     def all(self) -> List[UserFlags]:
         """List[:class:`UserFlags`]: Returns all public flags the user has."""
